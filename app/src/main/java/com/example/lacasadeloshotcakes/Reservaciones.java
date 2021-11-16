8537ad9bfef73651827f1e0e2147130413db42e1
MoniValencia <79213578+MoniValencia@users.noreply.github.com>
2021-11-16 14:59:53 -0600

    PRIMER COMMIT (REAL)

---
+++ app/src/main/java/com/example/lacasadeloshotcakes/Reservaciones.java
@@ -0,0 +1,14 @@
+package com.example.lacasadeloshotcakes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reservaciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservaciones);
+    }
+}
